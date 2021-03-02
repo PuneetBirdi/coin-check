d564d4df540b04b420f39653a12876870248f936
Puneet <birdipuneet@gmail.com>
2021-03-02 16:24:46 -0500

    Added websocket connection for Level2 orderbook data.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import { BrowserRouter as Router } from 'react-router-dom';
 import { formatChartData } from './utils/formatData'
 import axios from 'axios';
@@ -14,42 +14,63 @@ import styled from 'styled-components'
 
 const App = () => {
   const [tickerData, setTickerData] = useState("");
+  const [level2Data, setLevel2Data] = useState("");
   const [historicalData, setHistoricalData] = useState("")
   const [connectionStatus, setConnectionStatus] = useState(false)
   const [loading, setLoading] = useState(true)
 
   useEffect(async () => {
-        try {
-          const res = await axios.get(
-            "https://api.pro.coinbase.com/products/BTC-USD/candles?granularity=300"
-          );
 
-          const formattedData = await formatChartData(res.data)
-          setHistoricalData(formattedData)
-          setLoading(false)
-        } catch (error) {
-          console.log(error)
-        }
-        const subscribe = {
-          type: "subscribe",
-          channels: [{ name: "ticker", product_ids: ["BTC-USD"] }],
-        };
-        const ws = new WebSocket(
-          "wss://ws-feed-public.sandbox.pro.coinbase.com"
-        );
+  //Get historical price data from REST-API
+    try {
+      const res = await axios.get(
+        "https://api.pro.coinbase.com/products/BTC-USD/candles?granularity=60"
+      );
 
-        ws.onopen = () => {
-          ws.send(JSON.stringify(subscribe));
-        };
+      const formattedData = await formatChartData(res.data)
+      setHistoricalData(formattedData)
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+    }
 
-        ws.onmessage = (e) => {
-          setConnectionStatus(true)
-          setTickerData(JSON.parse(e.data));
-        };
+    
+  //Connect and subscribe to the appropriate websocket channels.
+    const subscribe = {
+      type: "subscribe",
+      channels: [
+        { name: "ticker", product_ids: ["BTC-USD"] },
+        { name: "level2", product_ids: ["BTC-USD"] },
+      ],
+    };
+    const ws = new WebSocket(
+      "wss://ws-feed-public.sandbox.pro.coinbase.com"
+    );
+    
+    //Send subscription message
+    ws.onopen = () => {
+      ws.send(JSON.stringify(subscribe));
+    };
 
-        ws.onerror = (e) =>{
-          setConnectionStatus(false)
-        }
+    //When message received, parse and push into the appropriate arrays depending on if it's ticker data, initial level2 data or a level2 data update.
+    ws.onmessage = (e) => {
+      setConnectionStatus(true)
+
+      const response = (JSON.parse(e.data));
+
+      if(response.type === "ticker"){
+        setTickerData(response);
+      }else if(response.type === 'snapshot'){
+        setLevel2Data(response)
+      }else if(response.type === 'l2update'){
+        //do this.
+      }
+    };
+
+    //If the socket receives and error, set connection status to false, so that an error can be displayed in the UI.
+    ws.onerror = (e) =>{
+      setConnectionStatus(false)
+    }
   }, [])
 
   return (
