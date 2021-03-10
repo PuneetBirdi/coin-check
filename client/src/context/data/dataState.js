ccebb9619b0cecf1ca1ad6dde034324bf075a842
Puneet <birdipuneet@gmail.com>
2021-03-09 20:57:00 -0500

    Created function in context-action to fetch historical product candle data.

--- client/src/context/data/dataState.js
+++ client/src/context/data/dataState.js
@@ -5,10 +5,12 @@ import DataReducer from './dataReducer';
 import {
     CONNECT_TO_SOCKET,
     HANDLE_FEED,
-    GET_TICKER_DATA,
+    GET_HISTORICAL_DATA,
     SET_LOADING,
-    SET_CONNECTION_STATUS
+    SET_CONNECTION_STATUS,
+    DATA_ERROR
 } from '../types';
+import {formatData} from '../../utils/formatData';
 
 //CONSTANTS
 const SUBSCRIBE = {
@@ -21,7 +23,7 @@ const SUBSCRIBE = {
 
 const WEBSOCKET_ADDRESS = "wss://ws-feed-public.sandbox.pro.coinbase.com";
 
-const COINBASE_REST_API = "https://api.pro.coinbase.com/products/BTC-USD/candles?granularity=60"
+const COINBASE_REST_API = "https://api.pro.coinbase.com"
 
 
 const DataState = (props) =>{
@@ -36,5 +38,39 @@ const DataState = (props) =>{
 
     const [state, dispatch] = useReducer(DataReducer, initialState)
 
-    
+
+    //Initial REST-API call for historical data
+    const getHistorical = async (product) =>{
+        setLoading(true);
+        //Make API request based on parameters that were passed in.
+        try {
+            const res = await axios.get(
+                `${COINBASE_REST_API}/products/BTC-USD/candles?granularity=60`
+            );
+
+            //Format response to appropriately fit charts
+            const formattedData = await formatChartData(res.data)
+
+            //write the formatted data to the context
+            dispatch({
+                type: GET_HISTORICAL_DATA,
+                payload: formattedData
+            })
+        } catch (error) {
+            dispatch({
+                type: DATA_ERROR,
+                payload: error
+            })
+        }
+    }
+
+
+    //Set Loading
+    const setLoading = () => {
+        dispatch({ type: SET_LOADING });
+    };
+
 }
+
+
+export default DataState;
